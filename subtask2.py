0a68e9eacac72d6d7c63c920e30e60754668db3a
Rumaisa Ashraf <Rumaisa.Ashraf@students.fh-wels.at>
2023-12-21 13:01:39 +0100

    add subtask2

---
+++ subtask2.py
@@ -0,0 +1,22 @@
+def CountSumMinMean():
+ n = 0
+ x = []
+ while True:
+ user_num = int(input("Enter a natural number (or -1 to
+stop): "))
+ if user_num == -1:
+ break
+ else:
+ n += 1
+ x.append(user_num)
+ if n == 0:
+ s = 0
+ m = -1
+ a = -1
+ else:
+ s = sum(x)
+ m = min(x)
+ a = s/n
+ print("The count is " + str(n)
++ ", the sum is " + str(s) + ", the minimum is " + str(m) + " and
+the mean is " + str(a))
